ed14bae9e78c24b88e6e64dc30dcd4ad1480bf8c
alphaoct26 <vaibhav05waghmare@gmail.com>
2024-08-17 19:16:23 +0530

    completed the regitration and deployed db in mongocloud

--- Api/controllers/auth.controllers.js
+++ Api/controllers/auth.controllers.js
@@ -1,13 +1,33 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 export const register = async(req, res) => {
     //db opreationcon
     const { username, email, password } = req.body;
-    // hashing the password by bcrypt
-    const hashpassword = await bcrypt.hash(password, 10);
-    console.log(hashpassword);
+    try {
+        // hashing the password by bcrypt
+        const hashpassword = await bcrypt.hash(password, 10);
+        console.log(hashpassword);
 
-    // creating a user which we in  db \
+        // creating a user which we in  db \
+
+        const newuser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashpassword,
+            },
+        });
+        console.log(newuser);
+        res.status(201).json({ message: "User created successfully" });
+    } catch (err) {
+        console.log(err);
+        res.status(201).json({ message: "user not created" });
+    }
 };
-export const login = (req, res) => {};
+
+export const login = (req, res) => {
+
+    const { username, password } = req.body;
+};
 
 export const logout = (req, res) => {};
